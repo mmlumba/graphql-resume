dfe7ee61bb3ae36c51f2cb71acb0224b6a4adb6b
mmlumba <mmlumba@gmail.com>
2023-07-26 11:19:32 -0400

    update resume info

--- src/resume.js
+++ src/resume.js
@@ -5,13 +5,17 @@ module.exports = {
             "role":"Front End Software Engineer",
             "company":"Demosphere",
             "startDate": "2018-09-13",
-            "endDate": "present",
+            "endDate": "2023-06-02",
             "responsibilities":[
-               "Developing Node.JS APIs for new back end services using GraphQL and MongoDB",
-               "Creating reusable UI components using React, Google’s Material Design, and Apollo GraphQL for use in future UI development",
+               "Implemented features for web applications in full stack JavaScript for core products: game and event scheduling systems, player and team registration systems",
+               "Redesigned UI of company mobile application using React Native and TypeScript",
+               "Modernized existing client-facing products with full stack JavaScript such as: client messaging system, theme customizer, and form builder",
+               "Conceptualized and implemented client side sports uniform design generator using React and GraphQL",
+               "Developed APIs for new back end services with Node.JS, GraphQL, and MongoDB",
+               "Created library of reusable UI components using React, Material UI, Apollo GraphQL",
                "Redesigned existing client-facing products such as a messaging system, theme customizer, and form builder with full stack JavaScript",
-               "Implemented client side sports uniform design generator using React and GraphQL",
-               "Organizing “code and coffee” sessions in order to promote knowledge sharing of tech best practices in the engineering team"
+               "Established company-wide code quality standards for front end projects to use in code reviews",
+               "Organized “code and coffee” sessions in order to promote knowledge sharing of tech best practices in the engineering team"
             ]
          },
         {
@@ -21,19 +25,21 @@ module.exports = {
            "startDate": "2016-12-01",
            "endDate": "2018-09-12",
            "responsibilities":[
-              "Refactored the front end of a healthcare administration system from Ext.JS to React and Material UI",
-              "Automated the implementation of Section 508 and U.S. Web Design standards across a suite of web applications using HTML, CSS, Ruby",
-              "Developed and maintain a web-based style guide for UI components in the Caseflow web application using React",
-              "Implemented new features for multiple applications using React and Ruby on Rails tech stack"
+              "Automated the implementation of Section 508 and U.S. Web Design standards across web applications",
+              "Proposed and executed open source solutions that reduced development costs and increased productivity",
+              "Created and maintained a web-based style guide for multiple UI components using React",
+              "Implemented new features in the benefits management system using React and Ruby on Rails tech stack",
+              "Supplemented existing features with unit tests using Rspec and Capybara "
            ]
         },
         {
            "type":"employment",
-           "role":"Sr. Application Developer Specialist",
+           "role":"Senior. Application Developer Specialist",
            "company":"NRECA",
            "startDate": "2014-06-02",
            "endDate": "2016-11-22",
            "responsibilities":[
+              "Built mockups for internal company expense website using Bootstrap & AngularJS",
               "Created scripts for deployment processes using Shell, Powershell, Ruby",
               "Implemented acceptance tests for server configurations using Ruby",
               "Automated server provisioning on an enterprise wide infrastructure using Puppet"
